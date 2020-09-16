108897bca77d3f1da7aae3247aae99970ac271b8
cermqm <63324300+cermqm@users.noreply.github.com>
2020-09-15 21:47:37 -0500

    fixed restaurant.js

--- models/restaurant/restaurant.js
+++ models/restaurant/restaurant.js
@@ -1,17 +1,17 @@
 module.exports = function(sequelize, DataTypes) {
-    const restaurant = sequelize.define("restaurant", {
-      name: {
-          type: DataTypes.STRING,
-          references: "name",
-          allowNull: false
-        },
-        location: {
-            type: DataTypes.STRING,
-            references: "location",
-            allowNull: false
-        }
-    });
+  const restaurant = sequelize.define("restaurant", {
+    name: {
+      type: DataTypes.STRING,
+      references: "name",
+      allowNull: false
+    },
+    location: {
+      type: DataTypes.STRING,
+      references: "location",
+      allowNull: false
+    }
+  });
   
-    return restaurant;
-  };
+  return restaurant;
+};
   
