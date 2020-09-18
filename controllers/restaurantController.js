9dc159067e7d8ccf2b79510e70545db69e804aaa
cermqm <63324300+cermqm@users.noreply.github.com>
2020-09-17 20:50:46 -0500

    added authetication and a few otehr details

--- controllers/restaurantController.js
+++ controllers/restaurantController.js
@@ -1,19 +1,27 @@
 // include express and all models
 const express = require("express");
 const router = express.Router();
+const isAuthenticated = require("../config/middleware/isAuthenticated");
 const db = require("../models");
 
+// route to see all restaurants
+router.get("/restaurant", isAuthenticated, (req, res) => {
+  db.Restaurant.findAll().then(data => {
+    res.render("restaurant", { data: data });
+  });
+});
+
 // route for add restaurant
-router.post("/restaurant/create", (req, res) => {
-  db.insertOne(req.body.name, () => {
-    res.redirect("/index");
+router.post("/create", isAuthenticated, (req, res) => {
+  db.Restaurant.insertOne(req.body.name, () => {
+    res.render("create", { data: data });
   });
 });
 
 // route to update restaurant
-router.post("/restaurant/update", (req, res) => {
-  db.updateOne(req.params.id, () => {
-    res.redirect("/index");
+router.post("/update", isAuthenticated, (req, res) => {
+  db.Restaurant.updateOne(req.params.id, () => {
+    res.render("update", { data: data });
   });
 });
 
