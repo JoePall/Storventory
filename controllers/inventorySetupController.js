e35bdd320ab8df90e22361e80840dd68463488f6
Josiah Powell <josiahpowell@outlook.com>
2020-09-17 21:30:46 -0500

    Adding force true to sequelize

--- controllers/inventorySetupController.js
+++ controllers/inventorySetupController.js
@@ -4,20 +4,9 @@ const isAuthenticated = require("../config/middleware/isAuthenticated");
 const db = require("../models");
 
 router.post("/inventory-setup", isAuthenticated, (req, res) => {
-  db.inventoryItem;
-  res.render("index", {
-    burgersOrderedByName: burgersOrderedByName,
-    burgersOrderedByDevoured: burgersOrderedByDevoured
+  db.InventoryItem.findAll().then(data => {
+    res.render("index", data);
   });
-});
-
-router.put("/api/burgers/", (req, res) => {
-  const devoured = req.body.devoured++;
-  const object = {
-    id: req.body.id,
-    devoured: devoured
-  };
-  burger.update(object, (err, data) => res.json(data));
 });
 
 module.exports = router;
