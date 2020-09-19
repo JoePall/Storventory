948b540f00ed148319a692df75741a9d9ce3d131
cermqm <63324300+cermqm@users.noreply.github.com>
2020-09-19 10:40:23 -0500

    working on InventoryManagementController.js /update

--- controllers/inventoryManagementController.js
+++ controllers/inventoryManagementController.js
@@ -3,24 +3,33 @@ module.exports = function(app) {
   const isAuthenticated = require("../config/middleware/isAuthenticated");
   const db = require("../models");
 
-  // route to see all restaurants
-  app.get("/restaurant", isAuthenticated, (req, res) => {
-    db.Restaurant.findAll().then(data => {
-      res.render("restaurant", { data: data });
+  // route to see all items in inventory
+  app.get("/inventoryItem/item", isAuthenticated, (req, res) => {
+    db.inventoryItem.findAll().then(data => {
+      res.render("item", { data: data });
     });
   });
 
-  // route for add restaurant
-  app.post("/create", isAuthenticated, (req, res) => {
-    db.Restaurant.insertOne(req.body.name, () => {
+  // route for add inventory item
+  app.post("/inventoryItem/create", isAuthenticated, (req, res) => {
+    db.Restaurant.insertOne({
+      quantity: req.body.quantity,
+      stockAmount: req.body.stockAmount,
+      stockNumber: req.body.stockNumber
+    }).then(data => {
       res.render("create", { data: data });
     });
   });
 
-  // route to update restaurant
-  app.post("/update", isAuthenticated, (req, res) => {
-    db.Restaurant.updateOne(req.params.id, () => {
-      res.render("update", { data: data });
+  // route to update inventory item
+  app.put("/inventoryItem/update", isAuthenticated, (req, res) => {
+    db.Restaurant.updateOne({
+      quantity: req.body.quantity,
+      stockAmount: req.body.stockAmount,
+      stockNumber: req.body.stockNumber,
+      where: { id: req.body.id }
+    }).then(data => {
+      res.render("create", { data: data });
     });
   });
 };
